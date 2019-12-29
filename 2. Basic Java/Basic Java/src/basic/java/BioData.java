149dae60adb44888b25fc3e8b674f3d5ba3bc3d5
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2019-12-29 17:07:55 +0600

    escape sequence

---
+++ 2. Basic Java/Basic Java/src/basic/java/BioData.java
@@ -0,0 +1,24 @@
+
+package basic.java;
+
+
+public class BioData {
+    public static void main(String[] args) {
+//        System.out.println("Nushrat Jahan Tuli");
+//        System.out.println("01615349666");
+//        System.out.println("B.Sc. in CSE");
+
+/*Escape Sequence*/
+
+       // System.out.println("Nushrat Jahan Tuli \n01615349666 \nB.Sc. in CSE");
+
+       /*tab take 4 gap between to digit*/
+       
+//        System.out.println("1 \t 2");
+//        System.out.println("3 \t 4");
+
+/*print double quotation*/
+        System.out.println(" \"JAVA Programming\" ");
+
+    }
+}

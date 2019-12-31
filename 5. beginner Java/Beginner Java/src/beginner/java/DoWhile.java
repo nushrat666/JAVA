000128a090d44a464750f2787e143a0c1ba8a657
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2019-12-31 23:38:40 +0600

    do while

---
+++ 5. beginner Java/Beginner Java/src/beginner/java/DoWhile.java
@@ -0,0 +1,51 @@
+
+package beginner.java;
+
+
+public class DoWhile {
+    public static void main(String[] args) {
+//        int i=1;
+//        do{
+//            System.out.println(i+" Bangladesh");
+//            i++;
+//        }while (i<=10);
+    
+    
+//     int i=1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while (i<=100);
+    
+
+// int i=1;
+//        do{
+//            System.out.println(i);
+//            i=i+2;
+//        }while (i<=100);
+
+//int i=2;
+//        do{
+//            System.out.println(i);
+//            i=i+2;
+//        }while (i<=100);
+
+
+/**
+ * difference between while loop and do while loop
+ */
+
+//int i=6;
+//        while(i<=5){
+//            System.out.println(i);
+//            i++;
+//    }//not printing any number
+
+
+int i=6;
+       do{
+         System.out.println(i);
+        i++;
+    }while (i<=5);
+    }
+}
--- 5. beginner Java/Beginner Java/src/beginner/java/WhileLoop.java
+++ 5. beginner Java/Beginner Java/src/beginner/java/WhileLoop.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package beginner.java;
 
-/**
- *
- * @author new
- */
+
 public class WhileLoop {
+    public static void main(String[] args) {
+//        int i=1;
+//        while(i<=10){
+//            System.out.println(i+" Bangladesh");
+//            i++;
+//        }
     
+//     int i=2;
+//        while(i<=100){
+//            System.out.println(i);
+//            i=i+2;
+//        }
+    
+
+int i=1;
+        while(i<=100){
+            System.out.println(i);
+            i=i+2;
+        }
+    }
+   
 }

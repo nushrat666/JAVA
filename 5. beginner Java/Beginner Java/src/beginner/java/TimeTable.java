0f2453ca5f38f92afa4d4f95242ac4662d74e1cf
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-01-05 15:59:39 +0600

    nested loop

--- 5. beginner Java/Beginner Java/src/beginner/java/TimeTable.java
+++ 5. beginner Java/Beginner Java/src/beginner/java/TimeTable.java
@@ -6,15 +6,20 @@ import java.util.Scanner;
 
 public class TimeTable {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
-        int num;
-        System.out.print("Enter any number : ");
-        num=input.nextInt();
-        for (int i = 1; i <= 10; i++) {
-            
-            System.out.println(num+"*"+i+" = "+num*i);
-        }
-        
-        
+//        Scanner input = new Scanner (System.in);
+//        int num;
+//        System.out.print("Enter any number : ");
+//        num=input.nextInt();
+//        for (int i = 1; i <= 10; i++) {
+//            
+//            System.out.println(num+"*"+i+" = "+num*i);
+//        }
+//        
+//        
+
+
+
+
+
     }
 }

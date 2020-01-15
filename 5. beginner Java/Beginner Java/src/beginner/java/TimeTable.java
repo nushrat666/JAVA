d42e95783e00753b3d76bae4f77f9428127ef9cd
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-01-15 11:19:15 +0600

    Nested for loop

--- 5. beginner Java/Beginner Java/src/beginner/java/TimeTable.java
+++ 5. beginner Java/Beginner Java/src/beginner/java/TimeTable.java
@@ -14,12 +14,24 @@ public class TimeTable {
 //            
 //            System.out.println(num+"*"+i+" = "+num*i);
 //        }
-//        
-//        
+       
 
+Scanner input= new Scanner(System.in);
+int m,n;
 
+        System.out.print("please enter initial number : ");
+        m=input.nextInt();
 
-
+ System.out.print("please enter last number : ");
+        n=input.nextInt();
+        
+        for (int i = m; i <= n; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i+" X "+j+" = "+i*j);
+            }
+            System.out.println("\n\n");
+            
+        }
 
     }
 }

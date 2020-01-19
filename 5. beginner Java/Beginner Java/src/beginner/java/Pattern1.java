5e2c43a5db0f89dccf42c01de3cfd3cb599908ce
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-01-19 13:34:05 +0600

    pattern

---
+++ 5. beginner Java/Beginner Java/src/beginner/java/Pattern1.java
@@ -0,0 +1,23 @@
+
+package beginner.java;
+
+import java.util.Scanner;
+
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int num;
+        System.out.print("Enter line number : ");
+        num=input.nextInt();
+        
+        for (int row = 1; row <= num; row++) {
+            for (int col = 1; col <=row; col++) {
+                System.out.print(" "+col);
+            }
+            System.out.println();
+            
+        }
+    }
+ 
+}

d76e4b953554c7678c2ccddfbd02adc2ef88cee2
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-01-20 11:11:10 +0600

    array

--- 5. beginner Java/Beginner Java/src/Array/Array4.java
+++ 5. beginner Java/Beginner Java/src/Array/Array4.java
@@ -14,12 +14,19 @@ public class Array4 {
         number[1][1]=50;
         number[1][2]=60;
       
-        System.out.println(number[0][0]);
-        System.out.println(number[0][1]);
-        System.out.println(number[0][2]);
-        System.out.println(number[1][0]);
-        System.out.println(number[1][1]);
-        System.out.println(number[1][2]);
+//        System.out.println(number[0][0]);
+//        System.out.println(number[0][1]);
+//        System.out.println(number[0][2]);
+//        System.out.println(number[1][0]);
+//        System.out.println(number[1][1]);
+//        System.out.println(number[1][2]);
+
+          for (int row = 0; row < 2; row++) {
+              for (int col = 0; col < 3; col++) {
+                  System.out.print(" "+number[row][col]);
+              }
+              System.out.println();
+        }
     }
    
 }

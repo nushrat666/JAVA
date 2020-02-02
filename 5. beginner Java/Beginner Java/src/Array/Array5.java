88faecce3e4c2aad0ac920841895223a3a99441a
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-02-02 15:42:26 +0600

    two dimensional array sum

--- 5. beginner Java/Beginner Java/src/Array/Array5.java
+++ 5. beginner Java/Beginner Java/src/Array/Array5.java
@@ -10,18 +10,18 @@ public class Array5 {
         int[][] B=new int[2][3];
         
         //getting input for A matrix
-        System.out.print("please enter values for A : ");
+        System.out.print("Enter elements for A matrix : ");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
-                  
+                System.out.printf("A[%d][%d]=",row,col);  
               A[row][col]=input.nextInt();
             }
         }
         //getting input for B Matrix
-        System.out.print("please enter values for B : ");
+        System.out.print("Enter elements for B matrix : ");
         for (int row = 0; row < 2; row++) {
             for (int col = 0; col < 3; col++) {
-                
+                System.out.printf("B[%d][%d] =",row,col);
                B[row][col]=input.nextInt();
             }
             
